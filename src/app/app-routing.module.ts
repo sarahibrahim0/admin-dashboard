58eb9b60052fb1f3ee60aebb355a114bc5092877
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2022-12-19 13:41:02 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ShellComponent, children: [
+      {path:'dashboard',component:DashboardComponent},
+      { path: 'categories', component: CategoriesComponent },
+      { path: 'categories/form', component: CategoriesFormComponent }
+
+
+    ]
+  },
+  { path: 'sidebar', component: SidebarComponent },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
+import { CategoriesFormComponent } from './pages/categories/categories-form/categories-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { ShellComponent } from './shared/shell/shell.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
-import { CategoriesFormComponent } from './pages/categories-form/categories-form.component';
+import { CommonModule} from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule , ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     ShellComponent,
+    DashboardComponent,
     CategoriesComponent,
     CategoriesFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

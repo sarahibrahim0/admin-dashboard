d57095554284aaea239a4046416d6137abf8f192
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2022-12-30 02:51:04 +0200

    9

--- src/app/pages/products/products-list/products-list.component.ts
+++ src/app/pages/products/products-list/products-list.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../../../models/product';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  products: [] = [];
+  products: Product[] = [];
 
   constructor() { }
 
@@ -15,6 +16,7 @@ export class ProductsListComponent implements OnInit {
   }
 
   editProduct(ProductId: string) { }
+  
   deleteProduct(ProductId: string) { }
 
 }
---
+++ src/app/services/products/products-service.service.ts
@@ -0,0 +1,38 @@
+import { Product } from './../../models/product';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product } from 'src/app/models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  api = `${environment.apiUrl}products`
+
+  postProduct(product : Product) : Observable<Product> {
+    return this.http.post<Product>(`${this.api}`, product);
+  }
+
+  getProducts() : Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.api}`);
+  }
+
+  getProductById(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.api}/${id}`);
+  }
+
+  deleteProduct(id: string): Observable<Product> {
+    return this.http.delete<Product>(`${this.api}/${id}`);
+  }
+
+  editProduct(id: string , product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.api}/${id}`, product);
+  }
+
+
+}

dfd6bcfabcb799a4d4cb4b8908782cb07cd9a565
Sarah Ibrahim <sarahibrahimabdelhamid@gmail.com>
2023-11-22 22:58:38 +0200

    last

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-// export const environment = {
-//   production: false,
-//   apiUrl :"https://dashboard-pnlv.onrender.com/api/v1/"
-// };
 export const environment = {
   production: false,
-  apiUrl :"http://localhost:3000/api/v1/"
+  apiUrl :"https://dashboard-pnlv.onrender.com/api/v1/"
 };
+// export const environment = {
+//   production: false,
+//   apiUrl :"http://localhost:3000/api/v1/"
+// };
 
 /*
 *  apiUrl : "https://dashboard-pnlv.onrender.com/api/v1/"
